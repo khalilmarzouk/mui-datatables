e65f8ebeafcf71b15c604d0c5688df6c767d67ac
Khalil Marzouk <K.MARZOUK@openbee.com>
2019-07-11 15:58:38 +0100

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
       }
     ]
   },
+  externals: {
+    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
